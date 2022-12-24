c69783490cd34951e65aa5af80df0e8c53bb274b
Automatic Linting <data@altamontco.com>
2022-12-24 16:12:05 -0600

    feat: wip - working towards UI

--- 01_display.py
+++ 01_display.py
@@ -1,92 +1,61 @@
 """
 Chat GPT prompt: 
-    - create a python program to show a window on the bottom right of the screen that says either "Play" with a green background or "Dodge" with a red background
-    - Can you make a table above with 3 rows?  In the first row, the left column item should say "Our Champion", the right column should say the contents of the variable our_champ.  The second row, the left column should say "Our/Theirs Agg. WR" and the right column should say "50-40".  The third row first column should say "Our Synergy" and the second column should say "45%"
+    - python gui create window that is 375px by 220px.  The window has a background color of light blue.  There are two columns.  The first has the text "Our Team" (font Arial, 25px size) followed by a thin gray line, followed by a 150px by 50px green box that contains the large text (30px) "50.7%".  The second column is the same, except the text says "Enemy Team" and the green box is instead orange.  Below the two columns is a button that says "Settings" and opens another window which allows the user to input a file path that is saved to a file.
+    - create a new python gui program that has a label with a background of green and text that says "Hello!".  In a new thread, change the background of that label to red and the text to say "New", then change it back every 5 seconds
+    - create a python gui that starts with the window in the bottom right corner of the screen
 """
 
 import tkinter as tk
 
 # Create the main window
-window = tk.Tk()
+root = tk.Tk()
+# root.geometry("425x200")
+# root.configure(bg="lightblue")
 
-# Set the window title
-window.title("Action Window")
+# Get the screen width and height
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
 
-# Set the window size
-window.geometry("200x150+1200+700")
+# Set the window size and position
+window_width = 425
+window_height = 200
+window_x = screen_width - window_width
+window_y = screen_height - window_height
+root.geometry("{}x{}+{}+{}".format(window_width, window_height, window_x, window_y))
+root.configure(bg="lightblue")
 
-# Create a frame to hold the buttons
-button_frame = tk.Frame(window)
+# Create the frame for the first column
+frame1 = tk.Frame(root, bg="lightblue")
+frame1.pack(side="left")
 
-# Create a button with the text "Capture" and a blue background
-capture_button = tk.Button(window, text="Capture", bg="blue", fg="white", width=20, height=5)
+# Create the label for the first column
+label1 = tk.Label(frame1, text="Our Team", font=("Arial", 25), bg="lightblue")
+label1.pack()
 
-# Display the capture button on the window
-capture_button.pack(side="top")
+# Create the separator for the first column
+separator1 = tk.Frame(frame1, height=2, bd=1, relief="sunken")
+separator1.pack(fill="x", padx=5, pady=5)
 
-# Create a button with the text "Play" and a green background
-play_button = tk.Button(button_frame, text="Play", bg="green", fg="white", width=20, height=5)
+# Create the green box for the first column
+green_box1 = tk.Label(frame1, text="50.7%", bg="green", font=("Arial", 30), width=9, height=2)
+green_box1.pack()
 
-# Create a button with the text "Dodge" and a red background
-dodge_button = tk.Button(button_frame, text="Dodge", bg="red", fg="white", width=20, height=5)
+# Create the frame for the second column
+frame2 = tk.Frame(root, bg="lightblue")
+frame2.pack(side="right")
 
-# Display the buttons on the frame
-play_button.pack(side="left")
-dodge_button.pack(side="right")
+# Create the label for the second column
+label2 = tk.Label(frame2, text="Enemy Team", font=("Arial", 25), bg="lightblue")
+label2.pack()
 
-# Display the frame on the window
-button_frame.pack(side="bottom")
+# Create the separator for the second column
+separator2 = tk.Frame(frame2, height=2, bd=1, relief="sunken")
+separator2.pack(fill="x", padx=5, pady=5)
 
-# Create a frame to hold the table
-table_frame = tk.Frame(window)
+# Create the orange box for the second column
+orange_box2 = tk.Label(frame2, text="50.7%", bg="orange", font=("Arial", 30), width=9, height=2)
+orange_box2.pack()
 
-# Create the first row of the table
-row1 = tk.Frame(table_frame)
-
-# Create the left column of the first row
-col1 = tk.Label(row1, text="Our Champion", width=15)
-
-# Create the right column of the first row
-our_champ = "Diana"
-col2 = tk.Label(row1, text=our_champ, width=15)
-
-# Display the columns in the first row
-col1.pack(side="left")
-col2.pack(side="right")
-
-# Create the second row of the table
-row2 = tk.Frame(table_frame)
-
-# Create the left column of the second row
-col3 = tk.Label(row2, text="Our/Theirs Agg. WR", width=15)
-
-# Create the right column of the second row
-col4 = tk.Label(row2, text="50-40", width=15)
-
-# Display the columns in the second row
-col3.pack(side="left")
-col4.pack(side="right")
-
-# Create the third row of the table
-row3 = tk.Frame(table_frame)
-
-# Create the left column of the third row
-col5 = tk.Label(row3, text="Our Synergy", width=15)
-
-# Create the right column of the third row
-col6 = tk.Label(row3, text="45%", width=15)
-
-# Display the columns in the third row
-col5.pack(side="left")
-col6.pack(side="right")
-
-# Display the rows in the frame
-row1.pack()
-row2.pack()
-row3.pack()
-
-# Display the frame on the window
-table_frame.pack()
 
 # Run the main loop
-window.mainloop()
+root.mainloop()
--- main.py
+++ main.py
@@ -7,12 +7,18 @@ current_lobby = {
     "us" : [],
     "them": [],
 }
-while(1):
+
+loop_check = True
+
+while(loop_check):
     new_lobby = client.read_lobby()
     if new_lobby != current_lobby:
         print()
         print("Our chances:", stats.check_synergies(new_lobby["us"]))
         print("Their chances:", stats.check_synergies(new_lobby["them"]))
         print()
+
+        if len(new_lobby["us"]) == 5 and len(new_lobby["them"]) == 5:
+            loop_check = False
 
     time.sleep(5)
